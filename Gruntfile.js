c02b04ca20fa7bbb4194b63e6cbeb2a0d5420fb7
iznyn <mahthunis@gmail.com>
2015-04-06 08:07:21 +0700

    Progress #1
    Create javascript for handling slider in image preview

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            //test
+            test: {
+                src: [
+                    'src/imagepreviewslider.js'
+                ],
+                dest: 'test/js/imagepreviewslider.js',
+            },
+        },
+        
+        uglify: {
+            main: {
+                src: 'src/imagepreviewslider.js',
+                dest: 'src/imagepreviewslider.min.js'
+            },
+            test: {
+                src: 'test/js/imagepreviewslider.js',
+                dest: 'test/js/imagepreviewslider.min.js'
+            },
+        },
+        less: {
+            main: {
+                options: {
+                    paths: [
+                        "test/css/less"
+                    ],
+                    compress: false
+                },
+                files: {
+                    //community
+                    "test/css/style.css": "test/css/less/main.less",
+                }
+            }
+        },
+        watch: {
+            js_concat: {
+                files: ['src/**/*.js'],
+                tasks: ['concat'],
+                options: {
+                    spawn: false,
+                },
+            },
+            js_uglify: {
+                files: ['src/**/*.js'],
+                tasks: ['uglify'],
+                options: {
+                    spawn: false,
+                },
+            },
+            doless: {
+                files: ['test/css/**/*.less'],
+                tasks: ['less'],
+                options: {
+                    spawn: false,
+                },
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.registerTask('default', ['concat','uglify','watch','less']);
+
+};
